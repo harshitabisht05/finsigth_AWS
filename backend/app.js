8a3fea87addd7f3513ddf3a01dc528fc87c38a4f
harshitabisht05 <harshitabisht0515@gmail.com>
2026-02-21 02:20:47 +0530

    commits

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,9 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const routes = require("./routes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-app.use(express.json());
-const app = express();
 
+const app = express();
+app.use(express.json());
 app.use(helmet());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
